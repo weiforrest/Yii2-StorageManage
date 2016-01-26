8f649f6847809613418f7a6f02332b2fdb81e4f3
forrest <max@forrestdeMacBook-Air.local>
2016-01-26 19:09:40 +0800

    remove the display of card_id

--- views/card/index.php
+++ views/card/index.php
@@ -25,7 +25,6 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'card_id',
             'name',
             'card_number',
 
